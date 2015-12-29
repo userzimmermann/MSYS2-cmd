f6ae068ea15c4390cc76281e29d4fd9c508d1aa8
Stefan Zimmermann <zimmermann.code@gmail.com>
2015-12-29 21:34:00 +0100

    added mingw32\bash.bat and mingw64\bash.bat

---
+++ mingw64/bash.bat
@@ -0,0 +1,31 @@
+@echo off
+REM CMD shell integration for MSYS2
+REM
+REM Copyright (C) 2015 Stefan Zimmermann <zimmermann.code@gmail.com>
+REM
+REM Licensed under the Apache License, Version 2.0
+
+setlocal EnableExtensions || exit /b
+
+if "%~1" == "/?" (
+    echo Runs MSYS2 bash.exe in MINGW64 mode.
+    echo.
+    echo BASH [args ...]
+    echo.
+    echo First starts a non-interactive bash.exe with --login flag,
+    echo which then switches to current working directory
+    echo and runs a new bash.exe instance with the given arguments.
+    echo.
+    echo If no arguments are given, a new shell window will be started.
+    echo Call BASH -i to run an interactive bash.exe in current shell window.
+    exit /b 0
+)
+
+set MSYSTEM=MINGW64
+if "%~1" == "" (
+    start bash.exe --login -c 'cd "%cd%"; bash'
+) else (
+    bash.exe --login -c 'cd "%cd%"; bash %*'
+)
+
+endlocal

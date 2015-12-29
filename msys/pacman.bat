48853863a688c6a3ac4cf52f75026a0a1c54dd84
Stefan Zimmermann <zimmermann.code@gmail.com>
2015-12-29 21:31:08 +0100

    added /? text to msys\pacman.bat

--- msys/pacman.bat
+++ msys/pacman.bat
@@ -5,4 +5,19 @@ REM Copyright (C) 2015 Stefan Zimmermann <zimmermann.code@gmail.com>
 REM
 REM Licensed under the Apache License, Version 2.0
 
-%~dp0\bash.bat -c "pacman --color auto %*"
+setlocal EnableExtensions || exit /b
+
+if "%~1" == "/?" (
+    echo Runs MSYS2 pacman.exe via bash.exe
+    echo.
+    echo PACMAN [args ...]
+    echo.
+    echo First starts a non-interactive bash.exe with --login flag,
+    echo which then switches to current working directory
+    echo and runs pacman.exe with the given arguments.
+    exit /b 0
+)
+
+%~dp0bash.bat -c "pacman --color auto %*"
+
+endlocal

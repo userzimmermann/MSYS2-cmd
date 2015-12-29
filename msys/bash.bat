935df0039ee960bfe301d0daa5beb79e3e9291e1
Stefan Zimmermann <zimmermann.code@gmail.com>
2015-12-29 21:28:50 +0100

    added /? text and %cd% delegation to msys\bash.bat

--- msys/bash.bat
+++ msys/bash.bat
@@ -5,13 +5,27 @@ REM Copyright (C) 2015 Stefan Zimmermann <zimmermann.code@gmail.com>
 REM
 REM Licensed under the Apache License, Version 2.0
 
-setlocal
+setlocal EnableExtensions || exit /b
+
+if "%~1" == "/?" (
+    echo Runs MSYS2 bash.exe in MSYS mode.
+    echo.
+    echo BASH [args ...]
+    echo.
+    echo First starts a non-interactive bash.exe with --login flag,
+    echo which then switches to current working directory
+    echo and runs a new bash.exe instance with the given arguments.
+    echo.
+    echo If no arguments are given, a new shell window will be started.
+    echo Call BASH -i to run an interactive bash.exe in current shell window.
+    exit /b 0
+)
 
 set MSYSTEM=MSYS
 if "%~1" == "" (
-    start bash.exe --login
+    start bash.exe --login -c 'cd "%cd%" ^&^& bash'
 ) else (
-    bash.exe --login %*
+    bash.exe --login -c 'cd "%cd%" ^&^& bash %*'
 )
 
 endlocal
